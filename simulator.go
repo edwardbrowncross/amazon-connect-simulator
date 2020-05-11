5b59db8a2ac3414ce05381eaa395fae47c4b6050
Edward Browncross <edward.browncross@infinityworks.com>
2020-05-11 23:59:57 +0100

    simulator comments

--- simulator.go
+++ simulator.go
@@ -13,6 +13,7 @@ import (
 	"github.com/edwardbrowncross/amazon-connect-simulator/module"
 )
 
+// CallSimulator is capable of starting new simulated call flows.
 type CallSimulator struct {
 	lambdas      map[string]interface{}
 	flows        map[string]flow.Flow
@@ -20,7 +21,9 @@ type CallSimulator struct {
 	startingFlow *flow.Flow
 }
 
-func NewCallSimulator() CallSimulator {
+// New creates a new call simulator.
+// It is created blank and must be set up using its attached methods.
+func New() CallSimulator {
 	return CallSimulator{
 		lambdas: map[string]interface{}{},
 		flows:   map[string]flow.Flow{},
@@ -28,6 +31,8 @@ func NewCallSimulator() CallSimulator {
 	}
 }
 
+// LoadFlow loads an unmarshalled call flow into the simulator.
+// Do this with all flows that form part of your call flows before starting a call.
 func (cs *CallSimulator) LoadFlow(flow flow.Flow) {
 	cs.flows[flow.Metadata.Name] = flow
 	for _, m := range flow.Modules {
@@ -35,6 +40,8 @@ func (cs *CallSimulator) LoadFlow(flow flow.Flow) {
 	}
 }
 
+// LoadFlowJSON takes a byte array containing a json file exported from Amazon Connect.
+// It does the same thing as LoadFlow, except that it does the unmarshalling for you.
 func (cs *CallSimulator) LoadFlowJSON(bytes []byte) error {
 	f := flow.Flow{}
 	err := json.Unmarshal(bytes, &f)
@@ -45,6 +52,10 @@ func (cs *CallSimulator) LoadFlowJSON(bytes []byte) error {
 	return nil
 }
 
+// RegisterLambda specifies how external lambda invocations will be handled.
+// name is a string that forms part of the lambda's ARN (such as its name).
+// fn is function like handle(context.Context, struct) (struct, error). It will be passed an Amazon Connect lambda event.
+// You must specify a function for each external lambda invocation before starting a simulated call.
 func (cs *CallSimulator) RegisterLambda(name string, fn interface{}) error {
 	err := validateLambda(fn)
 	if err != nil {
@@ -54,6 +65,9 @@ func (cs *CallSimulator) RegisterLambda(name string, fn interface{}) error {
 	return nil
 }
 
+// SetStartingFlow specifies the name of the flow that should be run when a new call comes in.
+// The name is the full name given to the flow in the Amazon Connect ui.
+// You must run this once before starting a simulated call.
 func (cs *CallSimulator) SetStartingFlow(flowName string) error {
 	f, ok := cs.flows[flowName]
 	if !ok {
@@ -63,6 +77,8 @@ func (cs *CallSimulator) SetStartingFlow(flowName string) error {
 	return nil
 }
 
+// StartCall starts a new call asynchronously and returns a Call object for interacting with that call.
+// Many independent calls can be spawned from one simulator.
 func (cs *CallSimulator) StartCall(config call.Config) (call.Call, error) {
 	if cs.startingFlow == nil {
 		return call.Call{}, errors.New("no starting flow set. Call SetStartingFlow before starting a call")
