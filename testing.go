d1c88c64e2d2a76c17f3e7d3a023623d1da4e8dd
Edward Browncross <edward.browncross@infinityworks.com>
2020-05-17 16:39:54 +0100

    use event channel for message assertions

--- testing.go
+++ testing.go
@@ -4,22 +4,50 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/edwardbrowncross/amazon-connect-simulator/event"
 )
 
 // TestHelper provides utility methods for asserting behavior of an ongoing call.
 type TestHelper struct {
-	t *testing.T
-	c Call
+	t    *testing.T
+	c    Call
+	evts <-chan event.Event
 }
 
 // NewTestHelper creates a new TestHelper wrapping an ongoing call.
 // TestHelper provides utility methods for asserting behavior of an ongoing call.
 func NewTestHelper(t *testing.T, c Call) TestHelper {
-	return TestHelper{t, c}
+	evts := make(chan event.Event)
+	go func() {
+		es := make([]event.Event, 0)
+		outCh := func() chan<- event.Event {
+			if len(es) == 0 {
+				return nil
+			}
+			return evts
+		}
+		eNext := func() event.Event {
+			if len(es) == 0 {
+				return nil
+			}
+			return es[0]
+		}
+		for {
+			select {
+			case <-c.O:
+			case e := <-c.Evt:
+				es = append(es, e)
+			case outCh() <- eNext():
+				es = es[1:]
+			}
+		}
+	}()
+	return TestHelper{t, c, evts}
 }
 
 // ToEnter sends the given string as either a numeric entry or as an option selection.
-// If not all characters can be sent, it errors the test.
+// If not all characters can be sent, or more characters are required, it errors the test.
 func (th *TestHelper) ToEnter(input string) {
 	for i, r := range input {
 		select {
@@ -40,13 +68,20 @@ func (th *TestHelper) ToEnter(input string) {
 // Message asserts that the next speech output matches the given string.
 // If the simulator does not output anything after a second, it is assumed it is stuck and it fatals the test.
 func (th *TestHelper) Message(msg string) {
-	select {
-	case got := <-th.c.O:
-		if got != msg {
-			th.t.Errorf("expected to hear message '%s' but got '%s'", msg, got)
+	for {
+		select {
+		case evt := <-th.evts:
+			switch evt.Type() {
+			case event.PromptType:
+				if got := evt.(event.PromptEvent).Text; got != msg {
+					th.t.Errorf("expected to hear message '%s' but got '%s'", msg, got)
+				}
+				return
+			}
+		case <-time.After(time.Second):
+			th.t.Fatalf("expected to hear message '%s' but it timed out", msg)
+			return
 		}
-	case <-time.After(time.Second):
-		th.t.Fatalf("expected to hear message '%s' but it timed out", msg)
 	}
 }
 
@@ -54,11 +89,14 @@ func (th *TestHelper) Message(msg string) {
 // If the simulator does not output anything after a second, it is assumed it is stuck and it fatals the test.
 func (th *TestHelper) MessageContaining(msg string) {
 	select {
-	case got := <-th.c.O:
-		if !strings.Contains(got, msg) {
-			th.t.Errorf("expected to hear message '%s' but got '%s'", msg, got)
+	case evt := <-th.evts:
+		switch evt.Type() {
+		case event.PromptType:
+			if got := evt.(event.PromptEvent).Text; !strings.Contains(got, msg) {
+				th.t.Errorf("expected to hear message containing '%s' but got '%s'", msg, got)
+			}
 		}
 	case <-time.After(time.Second):
-		th.t.Fatalf("expected to hear message '%s' but it timed out", msg)
+		th.t.Fatalf("expected to hear message containing '%s' but it timed out", msg)
 	}
 }
